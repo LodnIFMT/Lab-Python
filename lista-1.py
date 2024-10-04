6820ea8617bb2a7b5c047758109ed0afadff8df5
LodnIFMT <nascimento.lucas@estudante.ifmt.edu.br>
2024-10-04 00:12:21 -0400

    Exercicio da lista 1, com 16 completas e 4 incomepletas/não feitas

--- lista-1.py
+++ lista-1.py
@@ -83,25 +83,47 @@ def elevadoAoQuadrado():
 
 #13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
-
+def reajuste():
+    saldo = float(input('Digite o Saldo: '))
+    print(f'O seu Saldo com reajuste de 2% é: {round(saldo + (saldo * 2 / 100), 2)}')
 
 #14. Faça um programa que leia a base e a altura de um retângulo
 #    e imprima o perímetro (base + altura) e a área (base * altura / 2).
+def perimetroDoRetangulo():
+    print('---Perimetro de um Retangulo---')
+
+    base = float(input('Informe a Base: '))
+    altura = float(input('Informe a Altura: '))
+
+    print(f'O Perimetro do retangulo é {round(base + altura, 2)}')
+    print(f'A Área do Retangulo é {round(base * altura /2, 2)}')
 
 #15. Faça um programa que leia o valor de um produto, o percentual
 #    do desconto desejado e imprima o valor do desconto e o valor
 #    do produto subtraindo o desconto.
+def descontoDeProduto():
+    produto = float(input('Digite o valor do Produto: '))
+    #CONTINUAR
 
 #16. Faça um programa que calcule o reajuste do salário de um
 #    funcionário. Para isso, o programa deverá ler o salário atual
 #    do funcionário e ler o percentual de reajuste. Ao final imprimir
 #    o valor do novo salário.
+def calculoDeReajuste():
+    salario = float(input('Digite seu salario atual: '))
+    reajuste = int(input('Porcentual de reajuste: '))
 
+    print(f'O salario que antes era de {salario}')
+    print(f'Com {reajuste}% de reaujuste\nAgora seu salario é {round(salario + (salario * reajuste /100) ,2)}')
+          
 #17. Faça um programa que calcule a conversão entre graus centígrados
 #    e Fahrenheit. Para isso, leia o valor em centígrados e calcule
 #    com base na fórmula a seguir. Após calcular o programa deve
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
+def converterCentigrados():
+    centigrados = int(input('Informe a temperatura em centigrados: '))
+    print(f'A temperatura convertida para Fahrenheit é {(9 * centigrados + 160) /5}')
 
 #18. Faça um programa que calcule a quantidade de litros de combustível
 #    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
@@ -139,4 +161,9 @@ def elevadoAoQuadrado():
 #umQuarto()
 #realAritmetica()
 #operacoesBasicas()
-elevadoAoQuadrado()
+#elevadoAoQuadrado()
+#reajuste()
+#perimetroDoRetangulo()
+#descontoDeProduto()
+#calculoDeReajuste()
+converterCentigrados()
